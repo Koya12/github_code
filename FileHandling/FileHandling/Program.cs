6863416ff3dc4a35f58d12f4db4fe364efde1e4d
Koya12 <95679160+Koya12@users.noreply.github.com>
2023-05-23 06:54:43 +0530

    Add files via upload

---
+++ FileHandling/FileHandling/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*FileStream fo = new FileStream("e:\\AWT(T145)\\file.txt", FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fo);
+            Console.WriteLine("Enter the string null for termination");
+
+            while (true) 
+            {
+                String str = Console.ReadLine();
+                if (string.IsNullOrEmpty(str))
+                    break;
+                sw.WriteLine(str);
+            }
+            sw.Flush();
+            fo.Close();
+
+            //reading file
+            FileStream fi = new FileStream("e:\\AWT(T145)\\file.txt", FileMode.OpenOrCreate);
+            StreamReader sr = new StreamReader(fi);
+            string str1;
+            Console.WriteLine("The contents of he file are: ");
+            while ((str1 = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(str1);
+            }
+            sr.Close(); 
+            fi.Close();
+            Console.ReadKey();
+
+            //copy a content
+            FileStream fr = new FileStream("e:\\AWT(T145)\\File1.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fr);
+
+            FileStream fw = new FileStream("e:\\Copy.txt", FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fw);
+            String str;
+            while((str = sr.ReadLine()) != null)
+            {
+                sw.WriteLine(str);
+            }
+
+            sw.Flush();
+            fr.Close();
+            fw.Close();
+            Console.ReadKey();*/
+
+            //to copy and paste image
+            FileStream fr = new FileStream("d://6th sem//cloud.png", FileMode.Open);
+            FileStream fw = new FileStream("d://cloud.png", FileMode.OpenOrCreate);
+
+            int i;
+            while((i = fr.ReadByte()) != -1) 
+            {
+              fw.WriteByte((byte)i);    
+            }
+            fr.Close();
+            fw.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringMethods/StringMethods/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string name = "C#";
+            string interpolation
+            string msg = $"Welcome to {name}";
+            Console.WriteLine(msg);*/
+
+            //length
+            string st = "Kalyani";
+            Console.WriteLine("String Length: "+st.Length);
+
+            Char[] ch = { 'C', 's', 'h', 'a', 'r', 'p' };
+            String str = new String(ch);
+            Console.WriteLine(str);
+
+            Char[] ch1 = str.ToCharArray();
+            for(int i=0; i<str.Length; i++) 
+            {
+                Console.WriteLine(ch[i]);
+            }
+
+            //Concat
+            String str1 = "C";
+            String str2 = "Sharp";
+            Console.WriteLine("String Concatenation: "+String.Concat(str1, str2));
+            //Comparsion
+            Console.WriteLine("Equals: "+str1.Equals(str2));
+            //dictionary
+            Console.WriteLine("Dictionary: "+str1.CompareTo(str2));
+
+            //Split
+            String str3 = "Welcome to C# programming";
+            String[] spearator = { " " };
+            //Int32 count = 3;
+            String[] strlist = str3.Split(spearator,StringSplitOptions.RemoveEmptyEntries);
+           foreach(String s in strlist) 
+            {
+                Console.WriteLine(s+"\n");
+            }
+
+
+        }
+    }
+}
